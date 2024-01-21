21ba5b5838c098cfbd5c9ecb102f74a675fed790
Johnycrown <77997654+Johnycrown@users.noreply.github.com>
2024-01-21 01:32:01 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+struct WordCounter {
+    text: String,
 }
+
+impl WordCounter {
+    fn new(text: &str) -> WordCounter {
+        WordCounter {
+            text: String::from(text),
+        }
+    }
+
+    fn count_words(&self) -> usize {
+        let words: Vec<&str> = self.text.split_whitespace().collect();
+        words.len()
+    }
+}
+
+fn main() {
+    
+    println!("Enter a text:");
+    let mut input_text = String::new();
+    io::stdin().read_line(&mut input_text).expect("Failed to read line");
+
+    let word_counter = WordCounter::new(input_text.trim());
+
+    let word_count = word_counter.count_words();
+
+    println!("Word count: {}", word_count);
+}
+
+

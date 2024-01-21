3af93de6f14ffe2ed3c796a66eb2fb11d36463bc
John <john.oladeji@slsbank.com>
2024-01-21 01:28:31 +0100

    implemented the logic to count words from a text

---
+++ target/debug/deps/word_counter-2bc4f01960f3104b.d
@@ -0,0 +1,5 @@
+/Users/user/Documents/rustCode/word_counter/target/debug/deps/word_counter-2bc4f01960f3104b: src/main.rs
+
+/Users/user/Documents/rustCode/word_counter/target/debug/deps/word_counter-2bc4f01960f3104b.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/word_counter.d
@@ -0,0 +1,1 @@
+/Users/user/Documents/rustCode/word_counter/target/debug/word_counter: /Users/user/Documents/rustCode/word_counter/src/main.rs
